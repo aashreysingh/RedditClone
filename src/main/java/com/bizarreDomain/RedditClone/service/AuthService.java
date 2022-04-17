4ea01f08244ee5144fd4e56be14b015496e81841
Aashrey Singh <aashreyofficial@gmail.com>
2022-04-17 19:28:22 +0530

    persisting signed-up user data in DB using repository

--- src/main/java/com/bizarreDomain/RedditClone/service/AuthService.java
+++ src/main/java/com/bizarreDomain/RedditClone/service/AuthService.java
@@ -2,18 +2,22 @@ package com.bizarreDomain.RedditClone.service;
 
 import com.bizarreDomain.RedditClone.dto.RegisterRequest;
 import com.bizarreDomain.RedditClone.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bizarreDomain.RedditClone.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
+    @Transactional
     public void signup(RegisterRequest registerRequest){
         User user = new User();
         user.setUsername(registerRequest.getUsername());
@@ -21,6 +25,8 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreatedDate(Instant.now());
         user.setEnabled(false);
+
+        userRepository.save(user);
     }
 
 }
