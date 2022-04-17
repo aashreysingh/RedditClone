41ce1b00c4c5e892c47fe2d9787edb952ef8574d
Aashrey Singh <aashreyofficial@gmail.com>
2022-04-17 19:17:59 +0530

    adding config file for security configuration

---
+++ src/main/java/com/bizarreDomain/RedditClone/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.bizarreDomain.RedditClone.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // to disable csrf protection because we are not using cookies/session to keep track of authorization, and we are using JWT & REST for authorization
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
