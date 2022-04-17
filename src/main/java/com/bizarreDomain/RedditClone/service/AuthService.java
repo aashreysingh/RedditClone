b6f5424fb0dca4582835d2171a0224ac009621bb
Aashrey Singh <aashreyofficial@gmail.com>
2022-04-17 19:35:22 +0530

    generating verification token and persisting it against the user for whom it got generated

--- src/main/java/com/bizarreDomain/RedditClone/service/AuthService.java
+++ src/main/java/com/bizarreDomain/RedditClone/service/AuthService.java
@@ -2,13 +2,16 @@ package com.bizarreDomain.RedditClone.service;
 
 import com.bizarreDomain.RedditClone.dto.RegisterRequest;
 import com.bizarreDomain.RedditClone.model.User;
+import com.bizarreDomain.RedditClone.model.VerificationToken;
 import com.bizarreDomain.RedditClone.repository.UserRepository;
+import com.bizarreDomain.RedditClone.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +19,7 @@ public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @Transactional
     public void signup(RegisterRequest registerRequest){
@@ -27,6 +31,19 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    // persisting token against user in DB so that later we can perform token look up in DB and enable the specific user to which token belongs
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 
 }
