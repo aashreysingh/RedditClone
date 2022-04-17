9b43f5fdb455d549679f0c65cb6393a4289c6dfd
Aashrey Singh <aashreyofficial@gmail.com>
2022-04-17 19:17:33 +0530

    adding service [AuthService] to signup

---
+++ src/main/java/com/bizarreDomain/RedditClone/service/AuthService.java
@@ -0,0 +1,25 @@
+package com.bizarreDomain.RedditClone.service;
+
+import com.bizarreDomain.RedditClone.dto.RegisterRequest;
+import com.bizarreDomain.RedditClone.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder
+    public void signup(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(registerRequest.getPassword());
+        user.setCreatedDate(Instant.now());
+        user.setEnabled(false);
+    }
+
+}
