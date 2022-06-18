433ab79f78fdbf53f2fa2e6364af24b2903d4a1e
Aashrey Singh <aashreyofficial@gmail.com>
2022-06-18 20:01:28 +0530

    payload for PostRequest & PostResponse

---
+++ src/main/java/com/bizarreDomain/RedditClone/dto/request/PostRequest.java
@@ -0,0 +1,18 @@
+package com.bizarreDomain.RedditClone.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class PostRequest {
+    private Long postId;
+    private String subredditName;
+    private String postName;
+    private String url;
+    private String description;
+}
