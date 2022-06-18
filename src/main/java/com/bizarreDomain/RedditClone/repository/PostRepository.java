20eb8a86e2d59d47ddb85f8584dcabaf539aca35
Aashrey Singh <aashreyofficial@gmail.com>
2022-06-18 20:02:29 +0530

    updated method for JPA & service interaction

--- src/main/java/com/bizarreDomain/RedditClone/repository/PostRepository.java
+++ src/main/java/com/bizarreDomain/RedditClone/repository/PostRepository.java
@@ -1,9 +1,18 @@
 package com.bizarreDomain.RedditClone.repository;
 
 import com.bizarreDomain.RedditClone.model.Post;
+import com.bizarreDomain.RedditClone.model.Subreddit;
+import com.bizarreDomain.RedditClone.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllBySubreddit(Subreddit subreddit);
+
+    List<Post> findAllByUser(User user);
+
 }
