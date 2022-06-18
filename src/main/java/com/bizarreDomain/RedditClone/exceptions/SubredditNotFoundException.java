5e24d67b8f02a09107570e4655ea32e4447e61f0
Aashrey Singh <aashreyofficial@gmail.com>
2022-06-18 20:00:04 +0530

    Exception for Subreddit Not Found

---
+++ src/main/java/com/bizarreDomain/RedditClone/exceptions/SubredditNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bizarreDomain.RedditClone.exceptions;
+
+public class SubredditNotFoundException extends RuntimeException {
+    public SubredditNotFoundException(String s) {
+        super(s);
+    }
+}
