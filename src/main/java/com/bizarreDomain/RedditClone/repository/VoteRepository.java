bcbf8e6affaaba733de9394693215664df4dead8
Aashrey Singh <aashreyofficial@gmail.com>
2022-07-01 22:44:25 +0530

    added repository for Vote

--- src/main/java/com/bizarreDomain/RedditClone/repository/VoteRepository.java
+++ src/main/java/com/bizarreDomain/RedditClone/repository/VoteRepository.java
@@ -1,9 +1,16 @@
 package com.bizarreDomain.RedditClone.repository;
 
+import com.bizarreDomain.RedditClone.model.Post;
+import com.bizarreDomain.RedditClone.model.User;
 import com.bizarreDomain.RedditClone.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User user);
+
 }
